07260738f6596ff2989b7316645c9f0f7b86dc88
Martin Guillon <martin.guillon@akylas.fr>
2019-05-15 17:03:22 +0200

    chore: fix ios native build

--- src-native/ios/build.sh
+++ src-native/ios/build.sh
@@ -3,8 +3,8 @@ set -o pipefail
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-SOURCE_NAME="OpenCVAdditions"
+SOURCE_NAME="Canvas"
 IOS_SOURCE_DIR="$CURRENT_DIR"
 
 cd $IOS_SOURCE_DIR
-xcodebuild -workspace OpenCVAdditions.xcworkspace -scheme universal
+xcodebuild -project Canvas.xcodeproj -scheme universal
