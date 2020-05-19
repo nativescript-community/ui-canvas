ef652d32eab4318a32f400dac3cdd84ba21c018d
Martin Guillon <martin.guillon@akylas.fr>
2020-05-19 17:07:58 +0200

    test: demo app

--- demo-vue/app/examples/Shapes.vue
+++ demo-vue/app/examples/Shapes.vue
@@ -7,22 +7,33 @@
                 <Button text="size" @tap="animateRectSize" />
                 <Button text="arc" @tap="animateArc" />
             </StackLayout>
-
         </ActionBar>
         <GridLayout rows="*,auto">
-            <CanvasView row="0" :density="density" ref="canvas" width="100%" backgroundColor="#44ff0000">
-                <Shapes>
-                    <Rectangle shadow="3 3 black" strokeColor="red" :fillColor="shapeColor" strokeWidth="6" :left="shapeLeft" top="10" :width="shapeLeft + '%'" height="50%" />
-                    <Arc color="yellow" :antiAlias="antiAlias" shadow="0 0 10 red" paintStyle="stroke" strokeCap="round" strokeWidth="10" left="20" top="20" :width="arcWidth" :height="arcWidth" startAngle="0" :sweepAngle="sweepAngle" />
-                    <Text  :antiAlias="antiAlias" color="green" top="50" text="test Text" fontSize="50" />
-                    <Line  :antiAlias="antiAlias" color="yellow"  startX="10" startY="10" stopX="505" stopY="50"  strokeCap="round"  strokeJoin="round" strokeWidth="8" dash="1 10 0"/>
-                    <Line  :antiAlias="antiAlias" color="#1C2738"  startX="10%" startY="50%" stopX="90%" stopY="50%"  strokeCap="round"  strokeJoin="round" strokeWidth="1" dash="1 3 0"/>
-                </Shapes>
+            <CanvasView row="0" :density="density" ref="canvas" width="100%" backgroundColor="#44ff0000" :hardwareAccelerated="hardwareAccelerated">
+                <Rectangle shadow="3 3 black" strokeColor="red" :fillColor="shapeColor" strokeWidth="6" :left="shapeLeft" top="10" :width="shapeLeft + '%'" height="50%" />
+                <Arc
+                    color="yellow"
+                    :antiAlias="antiAlias"
+                    shadow="0 0 10 red"
+                    paintStyle="stroke"
+                    strokeCap="round"
+                    strokeWidth="10"
+                    left="20"
+                    top="20"
+                    :width="arcWidth"
+                    :height="arcWidth"
+                    startAngle="0"
+                    :sweepAngle="sweepAngle"
+                />
+                <Text :antiAlias="antiAlias" color="green" top="50" text="test Text" fontSize="50" />
+                <Line :antiAlias="antiAlias" color="yellow" startX="10" startY="10" stopX="505" stopY="50" strokeCap="round" strokeJoin="round" strokeWidth="8" dash="1 10 0" />
+                <Line :antiAlias="antiAlias" color="#1C2738" startX="10%" startY="50%" stopX="90%" stopY="50%" strokeCap="round" strokeJoin="round" strokeWidth="1" dash="1 3 0" />
             </CanvasView>
-            <GridLayout row="1" columns="auto, *, auto" orientation="horizontal">
-                <Label col="0" text="density" verticalAlignment="center"/>
+            <GridLayout row="1" columns="auto, *, auto, auto" orientation="horizontal">
+                <Label col="0" text="density" verticalAlignment="center" />
                 <Slider col="1" height="40" v-model="density" minValue="0" maxValue="10" verticalAlignment="center" />
-                <Switch col="2" v-model="antiAlias"/>
+                <Switch col="2" v-model="antiAlias" />
+                <Switch col="3" v-model="hardwareAccelerated" />
             </GridLayout>
         </GridLayout>
     </Page>
@@ -43,8 +54,9 @@ export default class Simple extends Vue {
     shapeLeft = 10;
     sweepAngle = 0;
     arcWidth = Math.min(screen.mainScreen.widthDIPs, screen.mainScreen.heightDIPs) - 40;
-    density = screen.mainScreen.scale
-    antiAlias = true
+    density = screen.mainScreen.scale;
+    antiAlias = true;
+    hardwareAccelerated = true;
     static title: 'Shapes Example';
     mounted() {}
     onBack() {
