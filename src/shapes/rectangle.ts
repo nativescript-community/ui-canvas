a0356a4ac354db277e58ab1b215c7c08a9bd182c
Martin Guillon <martin.guillon@akylas.fr>
2020-10-02 16:03:09 +0200

    feat: borderRadius for Rectangle

--- src/shapes/rectangle.ts
+++ src/shapes/rectangle.ts
@@ -1,18 +1,23 @@
 import { Length, PercentLength, zeroLength } from '@nativescript/core/ui/styling/style-properties';
 import { layout } from '@nativescript/core/utils/utils';
 import { Canvas, Rect, RectF, createRectF } from '../canvas';
-import Shape, { lengthProperty, percentLengthProperty } from './shape';
+import Shape, { lengthProperty, numberProperty, percentLengthProperty } from './shape';
 
 export { Rect, RectF };
 export default class Rectangle extends Shape {
     drawOnCanvas(canvas: Canvas) {
         // console.log('Rectangle', 'drawOnCanvas');
-        canvas.drawRect(this.getRect(canvas), this.paint);
+        if (this.borderRadius) {
+            canvas.drawRoundRect(this.getRect(canvas), this.borderRadius, this.borderRadius, this.paint);
+        } else {
+            canvas.drawRect(this.getRect(canvas), this.paint);
+        }
     }
     @percentLengthProperty width: PercentLength;
     @percentLengthProperty height: PercentLength;
     @lengthProperty left: Length = zeroLength;
     @lengthProperty top: Length = zeroLength;
+    @numberProperty borderRadius: number;
 
     // getRect(canvas: Canvas) {
     //     const availableWidth = layout.toDevicePixels(canvas.getWidth());
